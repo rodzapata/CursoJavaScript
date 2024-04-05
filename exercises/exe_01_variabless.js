0eb54454c87f1ac77ea674eb3cd5cab21082b588
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2024-04-05 17:01:37 -0500

    agregue carpeta exercises

---
+++ exercises/exe_01_variabless.js
@@ -0,0 +1,21 @@
+let name="rodney ";
+let lastName="Zapata";
+let age=48;
+
+console.log(name);
+console.log(lastName);
+console.log(age);
+
+//creamos la variable utilizando POO
+let word= new String("my firt word");
+console.log(word);
+
+let wordMayuscula=  word.toUpperCase();
+console.log(wordMayuscula);
+
+let nameMayuscula = name.toUpperCase();
+console.log(nameMayuscula);
+
+//converir una frase en un arreglo
+arreglo=word.split(" ");
+console.log(arreglo);
