8a4fdfb3359a665ba67bcb3bfe205d1be7978301
Rodney Zapata <rodneyzapatapalacio@gmail.com>
2024-09-25 16:53:56 -0500

    prototipos

---
+++ exercises/mapeos.js
@@ -0,0 +1,6 @@
+let numeros=[4,9,16]
+let duplicado=numeros.map((x)=> x*2);
+let raices=numeros.map(Math.sqrt);
+console.log(numeros)
+console.log(duplicado);
+console.log(raices);
---
+++ exercises/prototipos.js
@@ -0,0 +1,12 @@
+numeros=[2,3,4];
+
+let duplicado=numeros.map(function (x){
+    return x*2;
+})
+
+
+let duplicado2=numeros.map((x) => x*2)
+console.log(numeros);
+console.log(duplicado)
+console.log(duplicado2)
+clar
